128c761f0b91ee47a61e5739b42f312fa6fd1bd1
rubinigor-star <rubin.igor@gmail.com>
2026-07-25 22:44:59 +0300

    fix: trace WOFF2 ticket fonts for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/*": [
-      "./node_modules/@fontsource/noto-sans/files/*.woff",
       "./node_modules/@fontsource/noto-sans/files/*.woff2",
-      "./node_modules/@fontsource/noto-sans-hebrew/files/*.woff",
       "./node_modules/@fontsource/noto-sans-hebrew/files/*.woff2",
     ],
   },
