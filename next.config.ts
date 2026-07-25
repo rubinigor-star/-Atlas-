4324c7580c6b1b25a481bde389b52361abda4ae9
rubinigor-star <rubin.igor@gmail.com>
2026-07-25 22:29:36 +0300

    fix: include PDF font assets in Vercel functions

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,14 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@fontsource/noto-sans/files/*.woff",
+      "./node_modules/@fontsource/noto-sans/files/*.woff2",
+      "./node_modules/@fontsource/noto-sans-hebrew/files/*.woff",
+      "./node_modules/@fontsource/noto-sans-hebrew/files/*.woff2",
+    ],
+  },
+};
+
 export default nextConfig;
