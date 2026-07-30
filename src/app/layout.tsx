c478424229e1ea9f91f6b77d7342daf7606b0895
rubinigor-star <rubin.igor@gmail.com>
2026-07-30 15:37:52 +0300

    Load platform admin styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "@fontsource/inter/700.css";
 import "@fontsource/noto-sans-hebrew/400.css";
 import "@fontsource/noto-sans-hebrew/700.css";
 import "./globals.css";
+import "./platform.css";
 import "./event-experience.css";
 import "./logo.css";
 import "./logo-variants.css";
