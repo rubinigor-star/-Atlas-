ede48720ebe796dd5e55942436ef3db124432b3f
rubinigor-star <rubin.igor@gmail.com>
2026-08-11 14:28:32 +0300

    Add promoter forgot password page

---
+++ src/app/promoter/forgot-password/page.tsx
@@ -0,0 +1,3 @@
+import { PromoterAuthForm } from "@/components/promoter-auth-form";
+export const dynamic = "force-dynamic";
+export default function PromoterForgotPasswordPage(){ return <PromoterAuthForm mode="forgot"/>; }
