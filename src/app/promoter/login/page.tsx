70ee905439489ab474de8da215156e032143cbc8
rubinigor-star <rubin.igor@gmail.com>
2026-08-11 14:28:11 +0300

    Add promoter login page

---
+++ src/app/promoter/login/page.tsx
@@ -0,0 +1,3 @@
+import { PromoterAuthForm } from "@/components/promoter-auth-form";
+export const dynamic = "force-dynamic";
+export default function PromoterLoginPage(){ return <PromoterAuthForm mode="login"/>; }
