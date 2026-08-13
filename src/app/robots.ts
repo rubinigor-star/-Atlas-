68d66b039577fc6def935d0ae2f178edf66d729a
rubinigor-star <rubin.igor@gmail.com>
2026-08-13 10:27:13 +0300

    seo: protect private and tracking routes from crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,33 @@
 import type { MetadataRoute } from "next";
 
-export default function robots():MetadataRoute.Robots{
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules:[
-      {userAgent:"*",allow:"/",disallow:["/office/","/admin/","/api/","/scanner/","/checkout"]},
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/office/",
+          "/admin/",
+          "/api/",
+          "/scanner/",
+          "/checkout",
+          "/account/",
+          "/orders/",
+          "/payments/",
+          "/platform/",
+          "/promoter/",
+          "/cancel-order/",
+          "/cancellation-email-preview/",
+          "/ticket-design-preview/",
+          "/p/",
+          "/g/",
+          "/s/",
+          "/t/",
+        ],
+      },
     ],
-    sitemap:"https://www.atlas-one.co/sitemap.xml",
-    host:"https://www.atlas-one.co",
+    sitemap: "https://www.atlas-one.co/sitemap.xml",
+    host: "https://www.atlas-one.co",
   };
 }
