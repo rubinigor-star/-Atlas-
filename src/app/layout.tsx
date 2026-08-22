2597e7b33b0975f5e5421ba8ef5bf5d31d26502d
rubinigor-star <rubin.igor@gmail.com>
2026-08-22 10:03:10 +0300

    style(accessibility): load global accessibility styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "@fontsource/inter/700.css";
 import "@fontsource/noto-sans-hebrew/400.css";
 import "@fontsource/noto-sans-hebrew/700.css";
 import "./globals.css";
+import "./accessibility.css";
 import "./platform.css";
 import "./event-experience.css";
 import "./event-detail-sticky.css";
